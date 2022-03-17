b8b31722b01d7aa9fd2f681d6b152b4564112942
Monika <monikaleoster@gmail.com>
2022-03-16 17:42:45 -0700

    test case updated

--- complete/src/test/java/com/mon/assignment/controller/TemperatureControllerTest.java
+++ complete/src/test/java/com/mon/assignment/controller/TemperatureControllerTest.java
@@ -1,10 +1,11 @@
-/*
 package com.mon.assignment.controller;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mon.assignment.model.request.NotificationReq;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,10 +22,45 @@ public class TemperatureControllerTest {
 	@Autowired
 	private MockMvc mvc;
 
+
 	@Test
 	public void postNotifications() throws Exception {
-		mvc.perform(MockMvcRequestBuilders.post("/notifications").accept(MediaType.APPLICATION_JSON))
+	    String reqJson = "{\n" +
+                "  \"repeatedNotification\": false,\n" +
+                "  \"srcTemps\": [\n" +
+                "    0,\n" +
+                "    0.5,\n" +
+                "    0.2,\n" +
+                "    0,\n" +
+                "    1.5,\n" +
+                "    0\n" +
+                "  ],\n" +
+                "  \"thermometerReq\": {\n" +
+                "    \"boilTemperatureSpec\": {\n" +
+                "      \"maxThreshHold\": 0.5,\n" +
+                "      \"minThreshHold\": 0.5,\n" +
+                "      \"temp\": 100\n" +
+                "    },\n" +
+                "    \"freezeTemperatureSpec\": {\n" +
+                "      \"maxThreshHold\": 0.5,\n" +
+                "      \"minThreshHold\": 0.5,\n" +
+                "      \"temp\": 0\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+        ObjectMapper mapper = new ObjectMapper();
+        NotificationReq notificationReq = mapper.readValue(reqJson, NotificationReq.class);
+		mvc.perform(MockMvcRequestBuilders.post("/notifications").content(asJsonString(notificationReq))  .contentType(MediaType.APPLICATION_JSON)
+				.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
 	}
+
+	private static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
-*/
+
